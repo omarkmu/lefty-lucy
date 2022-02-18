99702875811396ef61051ae277224924788b1e59
omarkmu <omarkmu@gmail.com>
2022-02-18 15:00:20 -0500

    feat: Add basic bg scroll implementation

--- public/main.js
+++ public/main.js
@@ -3,8 +3,15 @@
  * Modified by Omar M.
  */
 
+const MAX_WIDTH = 1454;
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 600;
+const SCROLL_PERCENT = 0.20;
+const MIN_SCROLL = -(MAX_WIDTH - CANVAS_WIDTH);
+const SCROLL_DELTA = 3;
+
+const LEFT_SCROLL_BORDER = CANVAS_WIDTH * SCROLL_PERCENT;
+const RIGHT_SCROLL_BORDER = CANVAS_WIDTH - LEFT_SCROLL_BORDER;
 
 let player, cursors
 let bg, platforms;
@@ -21,19 +28,21 @@ function create() {
     cursors = this.input.keyboard.createCursorKeys();
 
     // initialize background
-    bg = this.add.image(727, 300, 'bg');
+    bg = this.add.image(0, 0, 'bg');
+    bg.setOrigin(0, 0);
 
     // initialize platforms
     let platformDefs = [
         // ground platform
-        {x: 0, y: 570, w: CANVAS_WIDTH, h: 1}
+        {x: 0, y: 550, w: CANVAS_WIDTH, h: 1}
     ];
 
     platforms = this.physics.add.staticGroup();
     for (let i = 0; i < platformDefs.length; i++) {
         const info = platformDefs[i];
-        const sprite = platforms.create(info.x, info.y, null, null, true);
+        const sprite = platforms.create(info.x, info.y, null, null, false);
         sprite.displayWidth = 1;
+        sprite.setOrigin(0, 0);
 
         sprite.enableBody();
         sprite.setSize(info.w, info.h, 0)
@@ -41,6 +50,7 @@ function create() {
 
     // not how lucy will actually look, but fine for a mockup
     player = this.physics.add.sprite(100, 450, 'dude');
+    player.setOrigin(0, 0);
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
@@ -73,22 +83,37 @@ function update() {
     if (cursors.left.isDown) {
         player.setVelocityX(-160);
         player.anims.play('left', true);
+
+        // handling it with a scroll border right now, but it should ultimately be a "region"
+        // region implementation should help avoid player's choppiness while scrolling
+        if (player.x <= LEFT_SCROLL_BORDER && bg.x < 0) {
+            bg.x += SCROLL_DELTA;
+            player.x += SCROLL_DELTA;
+        }
+
+        if (bg.x > 0) {
+            bg.x = 0;
+        }
     }
     else if (cursors.right.isDown) {
         player.setVelocityX(160);
         player.anims.play('right', true);
+
+        if (player.x >= RIGHT_SCROLL_BORDER && bg.x > MIN_SCROLL) {
+            bg.x -= SCROLL_DELTA;
+            player.x -= SCROLL_DELTA;
+        }
+
+        if (bg.x < MIN_SCROLL) {
+            bg.x = MIN_SCROLL;
+        }
     } else {
         player.setVelocityX(0);
         player.anims.play('turn');
     }
 
-    if (cursors.down.isDown) {
-        console.log(player);
-        console.log(bg);
-    }
-
-    if (cursors.up.isDown /*&& player.body.touching.down*/) {
-        player.setVelocityY(-160);
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-300);
     }
 }
 
