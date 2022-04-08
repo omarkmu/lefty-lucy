a5b5a771cddd94f7f301753d6a0fa11f313ff105
omarkmu <omarkmu@gmail.com>
2022-04-08 11:45:16 -0400

    chore: Add esbuild dependency chore: Remove packaged minified Phaser file chore: Add bundled main.js to .gitignore

---
+++ esbuild.config.mjs
@@ -0,0 +1,22 @@
+// adapted from Obsidian plugin template: https://github.com/obsidianmd/obsidian-sample-plugin
+// Omar Muhammad
+
+import esbuild from 'esbuild'
+import process from 'process'
+
+const mode = process.argv[2]
+const prod = mode === 'production'
+const watch = mode === 'watch'
+
+esbuild.build({
+    entryPoints: ['src/main.ts'],
+    bundle: true,
+    watch: watch,
+    minify: prod,
+    treeShaking: true,
+    sourcemap: prod ? false : 'inline',
+    format: 'cjs',
+    target: 'es2016',
+    logLevel: 'info',
+    outfile: 'public/main.js',
+}).catch(() => process.exit(1))
